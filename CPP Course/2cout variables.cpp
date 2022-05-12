45f3f4d3f6398ff2e5331e40addb3fdc1dc60234
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-12 18:08:23 +0530

    Add files via upload

---
+++ CPP Course/2cout variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=18;
+    float avg=89.8;
+    cout<<"Hi, this is Anjali. "<< age << endl << avg;
+    system("PAUSE");
+    return 0;
+}

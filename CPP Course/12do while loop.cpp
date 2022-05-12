d1493ba1ea9b07224ea13774168f14e0a61a75a3
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-13 00:23:47 +0530

    Add files via upload

---
+++ CPP Course/12do while loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=-1, x;
+    
+    do
+    {
+        cout<<"Welcome to VS code!!"<<endl;
+        cin>>x;
+    }while(x!=i);
+    
+    return 0;
+}

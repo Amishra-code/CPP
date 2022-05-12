e7d48a232a66ba88cf7c7b466ba68adc19fa8cec
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-12 18:11:22 +0530

    Add files via upload

---
+++ CPP Course/9bool data type.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool human=1;
+
+    if(!human)
+        cout<<"you are not a human."<<endl; 
+    else
+        cout<<"you are a human."<<endl;
+        
+    system("PAUSE");
+    return 0;
+}

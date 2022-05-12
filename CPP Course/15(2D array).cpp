1047df88524a7963eac9ac5103e232dce3ba08c9
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-13 00:25:02 +0530

    Add files via upload

---
+++ CPP Course/15(2D array).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[2][3]={1, 2, 3, 4, 5};
+
+    cout<< a[0][0]<<" ";
+    cout<< a[0][1]<<" ";
+    cout<< a[0][2]<< endl;
+    cout<< a[1][0]<<" ";
+    cout<< a[1][1]<<" ";
+    cout<< a[1][2]<< endl;
+    
+    return 0;
+}

04a45f9704d3cfd8ddd81b46a47f3f1c74455067
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-12 18:08:47 +0530

    Add files via upload

---
+++ CPP Course/3age and average.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    float avg;
+    cout<< "enter age and average ."<<endl;
+    cin>> age >> avg;
+    cout<< "you have enter the age "<< age << endl << "you have entered the average " << avg;
+    return 0;
+}

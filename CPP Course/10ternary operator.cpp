1672246a93966f962759808cefcc36cccf3beea5
Amishra-code <85864787+Amishra-code@users.noreply.github.com>
2022-05-12 18:11:52 +0530

    Add files via upload

---
+++ CPP Course/10ternary operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int mark;
+    cout<<"enter your mark : ";
+    cin>>mark;
+
+    mark>=35? cout<<"you have passed the exam."<<endl : cout<<"you failed the exam."<<endl;
+        
+    system("PAUSE");
+    return 0;
+}

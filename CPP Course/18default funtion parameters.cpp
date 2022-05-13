2a2c0f0d1c974d15050f298076967d5136f279de
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-14 01:25:26 +0530

    Add files via upload

---
+++ CPP Course/18default funtion parameters.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int display(int a=-1, int b=-1, int c=-1)
+{
+    cout<<"A is : " <<a <<endl;
+    cout<<"B is : " <<b <<endl;
+    cout<<"C is : " <<c <<endl;
+  
+    return 0;
+}
+
+int main()
+{
+    display(10, 20);
+    return 0;
+}
+
+

6fd6530f0dbc38b0ed82cef2a8504e367067e876
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-15 12:25:33 +0530

    Add files via upload

---
+++ CPP Course/26pass by address.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void show(int *ptr);
+
+int main()
+{
+    int age = 18;
+
+    show(&age);
+
+    cout<< age << endl;
+
+    return 0;
+
+}
+
+void show(int *ptr)
+{
+    cout<< (*ptr) << endl;
+
+    *ptr = 100;
+
+}
+

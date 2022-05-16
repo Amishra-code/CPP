74a9a6da9c3c0e013336142c46aabafe0644f793
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-16 12:55:22 +0530

    Add files via upload

---
+++ CPP Course/40auto.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    auto age = 10;
+    
+    // auto i;    {give an error bcz we have not initialized the value so it can't fetch the datat type all by itself}
+    
+    cout<< age;
+
+    return 0;
+
+}

f1601d91ce7a4cd3bcaaa079dd9b9ad4bffbedb4
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-17 09:53:07 +0530

    Add files via upload

---
+++ CPP Course/41.range based for loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*int array[]={1, 2, 3, 4, 5}
+    
+    for(int var : array)
+    {
+        cout<<var<< endl;
+    }
+
+    return 0;*/
+
+}

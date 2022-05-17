b73d84f598fac95c137b0ffd2f1879e571110bce
Anjali Mishra <85864787+Amishra-code@users.noreply.github.com>
2022-05-18 02:25:24 +0530

    Add files via upload

---
+++ CPP Course/45class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+class HumanBeing
+{
+    public:
+    void display()
+    {
+        cout<<"I'm a human being.";
+    }
+};
+
+int main()
+{
+    HumanBeing anjali;
+    anjali.display();
+
+    return 0;
+
+}
